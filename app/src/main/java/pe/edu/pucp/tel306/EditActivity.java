1bdd8dc1498f81b92da4346f20e90117215fdbc6
Royer <r.yangali@pucp.pe>
2020-10-13 02:14:15 -0500

    edit Preferences

---
+++ app/src/main/java/pe/edu/pucp/tel306/EditActivity.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.tel306;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+        setTitle("Editar preferencias");
+
+    }
+}
