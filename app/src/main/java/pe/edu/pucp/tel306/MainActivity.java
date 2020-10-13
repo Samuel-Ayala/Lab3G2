4a56ecc28296afc76f5ca7fabdb33c7ebc7c00e0
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-10-13 10:37:39 -0500

    timer

--- app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
+++ app/src/main/java/pe/edu/pucp/tel306/MainActivity.java
@@ -18,9 +18,22 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 import pe.edu.pucp.tel306.ViewModels.TimerViewModel;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String[] curiosos = {"Los ojos hacen más ejercicio que las piernas.",
+            "Nuestro aroma es tan único como nuestras huellas digitales.",
+            "El corazón podría mover un coche.",
+            "En 30 minutos, el cuerpo humano libera suficiente calor como para hervir casi medio litro de agua.",
+            "¿Qué crees que crece más rápido en tu cuerpo? La respuesta no son las uñas. En realidad, el vello facial crece mucho más rápido que el de cualquier otra parte del cuerpo.",
+            "Los elefantes son maravillosos y a nuestros ojos, parecen gigantes. Sin embargo, pesan menos que la lengua de una ballena azul."};
+
+    private static final String[] concentracion = {"motivacion 1", "motivacion 2"};
+    private static final String[] msgDescanso = {"descanso 1", "descanso 2"};
+
+
     private ActionMode mactionMode;
     private static boolean pause = true;
     private static boolean isResting = false;
@@ -28,13 +41,25 @@ public class MainActivity extends AppCompatActivity {
     private static long rest = 5 * 60 * 1000;//maximo descanso
     private static int pomodoro = 4;//maximo ciclos
     private static long time = 25 * 60 * 1000; //tiempo instantaneo
-    private static int ciclo = 1;//tiempo instantaneo
+    private static int ciclo = 1;//ciclo instantaneo
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((TextView) findViewById(R.id.textViewdescanso)).setText("Tiempo de descanso: " + getTimeStr(rest));
+        ((TextView) findViewById(R.id.textViewCiclo)).setText("Ciclo Pomodoro " + String.valueOf(ciclo) + " de  " + String.valueOf(pomodoro));
         printTimer();
+        if (isResting) {
+            ((TextView) findViewById(R.id.textViewTimer)).setTextColor(Color.parseColor("#44B849"));
+            findViewById(R.id.textViewRest).setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.textViewRest).setVisibility(View.GONE);
+            ((TextView) findViewById(R.id.textViewTimer)).setTextColor(Color.parseColor("#292929"));
+        }
+
 
         if (pause) {
             findViewById(R.id.buttonPause).setVisibility(View.GONE);
@@ -76,6 +101,10 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 findViewById(R.id.textViewRest).setVisibility(View.GONE);
                 ((TextView) findViewById(R.id.textViewTimer)).setTextColor(Color.parseColor("#292929"));
+                ((TextView) findViewById(R.id.textViewCiclo)).setText("Ciclo Pomodoro 1 de " + String.valueOf(pomodoro));
+                ciclo = 1;
+                isResting = false;
+
                 pause = true;
                 findViewById(R.id.buttonPause).setVisibility(View.GONE);
                 findViewById(R.id.buttonPlay).setVisibility(View.VISIBLE);
@@ -92,7 +121,12 @@ public class MainActivity extends AppCompatActivity {
         timerViewModel.getContadorMutable().observe(this, new Observer<Long>() {
             @Override
             public void onChanged(Long lg) {
+
+                Random r = new Random();
+
+
                 if (lg == 0) {
+
                     if (!isResting) {
                         AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
                         ad.setMessage("Deten todo! Hora de descansar :)");
@@ -119,6 +153,9 @@ public class MainActivity extends AppCompatActivity {
                             findViewById(R.id.textViewRest).setVisibility(View.GONE);
                             ((TextView) findViewById(R.id.textViewTimer)).setTextColor(Color.parseColor("#292929"));
                             time = limit;
+                            ((TextView) findViewById(R.id.textViewCiclo)).setText("Ciclo Pomodoro 1 de " + String.valueOf(pomodoro));
+                            ciclo = 1;
+
                             pause = true;
                             findViewById(R.id.buttonPause).setVisibility(View.GONE);
                             findViewById(R.id.buttonPlay).setVisibility(View.VISIBLE);
@@ -126,7 +163,7 @@ public class MainActivity extends AppCompatActivity {
                             AlertDialog.Builder ad = new AlertDialog.Builder(MainActivity.this);
                             ad.setTitle("Felicidades!!!!");
                             String msg = "A continuación un dataso curioso:\n";
-
+                            msg = msg + curiosos[r.nextInt(curiosos.length)];
                             ad.setMessage(msg);
                             ad.setPositiveButton("WOW!", new DialogInterface.OnClickListener() {
                                 @Override
@@ -157,13 +194,20 @@ public class MainActivity extends AppCompatActivity {
                             time = limit;
 
                             TextView pomo = findViewById(R.id.textViewCiclo);
-                            pomo.setText("Ciclo Pomodoro " + String.valueOf(ciclo) + " de  "+String.valueOf(pomodoro));
+                            pomo.setText("Ciclo Pomodoro " + String.valueOf(ciclo) + " de  " + String.valueOf(pomodoro));
                         }
                         isResting = false;
                     }
 
 
                 } else {
+                    if (lg % (1000 * 5) == 0) {
+                        if (!isResting) {
+                            ((TextView) findViewById(R.id.textViewFrases)).setText(concentracion[r.nextInt(concentracion.length)]);
+                        } else {
+                            ((TextView) findViewById(R.id.textViewFrases)).setText(msgDescanso[r.nextInt(msgDescanso.length)]);
+                        }
+                    }
                     time = lg;
                 }
                 printTimer();
@@ -237,10 +281,11 @@ public class MainActivity extends AppCompatActivity {
             limit = newlimit;
             rest = newrest;
             pomodoro = newpomo;
-            ciclo=1;
+            ciclo = 1;
             ((TextView) findViewById(R.id.textViewdescanso)).setText("Tiempo de descanso: " + getTimeStr(rest));
             ((TextView) findViewById(R.id.textViewCiclo)).setText("Ciclo Pomodoro 1 de " + String.valueOf(pomodoro));
             time = limit;
+            isResting = false;
             printTimer();
         }
     }
@@ -291,13 +336,13 @@ public class MainActivity extends AppCompatActivity {
                     findViewById(R.id.textViewRest).setVisibility(View.GONE);
                     ((TextView) findViewById(R.id.textViewTimer)).setTextColor(Color.parseColor("#292929"));
                     limit = 25 * 60 * 1000;
-                    time=limit;
+                    time = limit;
                     rest = 5 * 60 * 1000;
-                    pomodoro=4;
-                    ciclo=1;
+                    pomodoro = 4;
+                    ciclo = 1;
                     ((TextView) findViewById(R.id.textViewdescanso)).setText("Tiempo de descanso: 05:00");
                     ((TextView) findViewById(R.id.textViewCiclo)).setText("Ciclo Pomodoro 1 de 4");
-
+                    isResting = false;
                     printTimer();
                     mode.finish();
                     return true;
